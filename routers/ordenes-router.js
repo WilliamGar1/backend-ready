5c3d9c6005522ebb2f7d4dfc533620d71331f77d
William Garcia <williamjbg06@hotmail.com>
2022-04-25 22:17:59 -0600

    rutas cuando se toma una orden

--- routers/ordenes-router.js
+++ routers/ordenes-router.js
@@ -19,6 +19,8 @@ router.get('/', (req, res) => {
     });
 });
 
+
+//actualizar datos de orden tomada
 router.put('/:id/taken', (req, res) => {
     ordenes.updateOne({
         _id: mongoose.Types.ObjectId(req.params.id)
@@ -35,6 +37,22 @@ router.put('/:id/taken', (req, res) => {
     })
     .catch(() => {
         res.send({update: false, msg: 'Error al tomar la orden'});
+        res.end();
+    });
+});
+
+
+//obtener una orden en especifico
+router.get('/:id', (req, res) => {
+    ordenes.find({
+        _id: mongoose.Types.ObjectId(req.params.id)
+    })
+    .then(data => {
+        res.send(data[0]);
+        res.end();
+    })
+    .catch(e => {
+        res.send(e);
         res.end();
     });
 })
