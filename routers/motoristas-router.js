3bfcebb764c22857ad26131fe211ba4718b63e8e
William Garcia <williamjbg06@hotmail.com>
2022-04-26 02:08:05 -0600

    rutas para el login

--- routers/motoristas-router.js
+++ routers/motoristas-router.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var motoristas = require('../models/motoristas');
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
     motoristas.find({})
@@ -16,6 +17,24 @@ router.get('/', (req, res) => {
 });
 
 
+router.post('/login', async function(req,res){
+    const {correo, pass} = req.body;
+
+    const Motorista = await motoristas.findOne({correo},{});
+
+    if (!Motorista){
+        return res.status(401).send('Correo incorrecto');
+    }
+    if (Motorista.contrasena !== pass){
+        return res.status(401).send('Contraseña Incorrecta');
+    } 
+
+    const token = jwt.sign({_id: Motorista._id}, 'Motoristakey');
+
+    const _id = Motorista._id;
+
+    return res.status(200).json({token, _id, acceso: true});
+});
 
 
 module.exports = router;
